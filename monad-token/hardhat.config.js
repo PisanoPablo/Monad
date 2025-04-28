ec84f34fdf1f0ccea948292b0d510c8cd26e926b
PisanoPablo <pisanopablo6@gmail.com>
2025-04-28 09:33:30 +0200

    FIRST COMMIT

--- monad-token/hardhat.config.js
+++ monad-token/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     monad: {
       url: "https://testnet-rpc.monad.xyz",
-      accounts: ["buffalo dose tube mechanic reopen adapt spot oval defense grunt ski squirrel"], // ⚠️ wallet de prueba
+      accounts: [""], // ⚠️ wallet de prueba
       chainId: 10143,
     }
   }
